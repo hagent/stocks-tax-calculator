4e67802db8d1bca13f33fbea2c592f671e026f84
Alexey Tomin <alextomin@list.ru>
2023-08-31 00:21:28 +0200

    something

--- taxes_calculator.js
+++ taxes_calculator.js
@@ -1,5 +1,5 @@
-import { promises as fs } from 'fs'
-import { parse } from 'date-fns'
+import { promises as fs } from "fs";
+import { parse } from "date-fns";
 
 // todo take it from api
 const stockSplitHistory = {
@@ -13,21 +13,21 @@ const stockSplitHistory = {
     date: new Date(2020, 7, 31),
     split: 5
   }]
-}
+};
 
 async function readRatesFile(file) {
-  const ratesFile = await fs.readFile(file, 'utf8');
-  const lines = ratesFile.split('\n').slice(2);
-  const rates = []
+  const ratesFile = await fs.readFile(file, "utf8");
+  const lines = ratesFile.split("\n").slice(2);
+  const rates = [];
   for (const line of lines) {
     if (line.trim().length === 0) break;
-    const [dateStr, , rateStr] = line.split(';');
+    const [dateStr, , rateStr] = line.split(";");
     // console.log({ line, rateStr })
     const rate = {
-      date: parse(dateStr, 'yyyyMMdd', new Date()),
-      rate: Number.parseFloat(rateStr.replace(',', '.'))
+      date: parse(dateStr, "yyyyMMdd", new Date()),
+      rate: Number.parseFloat(rateStr.replace(",", "."))
     };
-    rates.push(rate)
+    rates.push(rate);
   }
   return rates;
 }
@@ -35,10 +35,10 @@ async function readRatesFile(file) {
 async function combineRates() {
   // from https://www.nbp.pl/home.aspx?f=/kursy/arch_a.html
   const rates = [
-    ...await readRatesFile('archiwum_tab_a_2019.csv'),
-    ...await readRatesFile('archiwum_tab_a_2020.csv'),
-    ...await readRatesFile('archiwum_tab_a_2021.csv')
-  ]
+    ...await readRatesFile("archiwum_tab_a_2019.csv"),
+    ...await readRatesFile("archiwum_tab_a_2020.csv"),
+    ...await readRatesFile("archiwum_tab_a_2021.csv")
+  ];
   return rates;
 }
 
@@ -52,28 +52,27 @@ function findRate(rates, date) {
     prevRate = rate;
   }
   return 0;
+}
 
+function parseRevolutNumber(usd) {
+  return Number.parseFloat(usd.replace(",", ""));
 }
 
 function parseTransactionUsdValue(line) {
   const usd = /[+-]\$([\d.,]+)/.exec(line)?.[1];
   if (!usd) {
-    console.log('no usd', line);
-    throw new Error('no usd');
+    console.log("no usd", line);
+    throw new Error("no usd");
   }
   return parseRevolutNumber(usd);
 }
 
-function parseRevolutNumber(usd) {
-  return Number.parseFloat(usd.replace(',', ''));
-}
-
-function round(val, precision = 2) {
+function round2(val) {
   return Math.round(val * 100) / 100;
 }
 
 function getSplitMultiplier(company, date) {
-  const splits = stockSplitHistory[company]?.filter(spl => date < spl.date) ?? [];
+  const splits = stockSplitHistory[company]?.filter((spl) => date < spl.date) ?? [];
   if (splits.length === 0) return 1;
   const splitMultiplyer = splits.reduce((acc, split) => acc * split.split, 1);
   // console.log('splits multiply', company, date.toLocaleDateString(), shares, splitMultiplyer);
@@ -81,8 +80,8 @@ function getSplitMultiplier(company, date) {
 }
 
 function parseStock(type, line, date, rate) {
-  const usd = parseTransactionUsdValue(line)
-  const split = line.split(' ');
+  const usd = parseTransactionUsdValue(line);
+  const split = line.split(" ");
   const shares = parseRevolutNumber(split[2]);
   const company = split[1];
   return {
@@ -94,58 +93,55 @@ function parseStock(type, line, date, rate) {
     rate,
     line,
     usd,
-    pln: round(usd * rate)
-  }
+    pln: round2(usd * rate)
+  };
 }
 
 function parseDevidend(line, date, rate) {
-  const usd = parseTransactionUsdValue(line)
+  const usd = parseTransactionUsdValue(line);
   // console.log(line, ' pln=', usd * rate)
   return {
     date,
     rate,
     line,
     usd,
-    pln: round(usd * rate)
-  }
+    pln: round2(usd * rate)
+  };
 }
 
-async function parseTransactions(rates) {
-  const lines = (await fs.readFile('stocks_transactions.txt', 'utf8'))
-    .split('\n');
-  // .slice(0, 10);
+async function parseHtmlTransactions(rates, transactions) {
   let year;
   let date;
   let rate;
   const devidends = [];
   const stocks = [];
-  for (const line of lines) {
+  for (const line of transactions) {
     // const line = l.trim();
     // console.log(JSON.stringify(line));
     switch (true) {
       case /^\d{4}$/.test(line):
-        year = Number.parseInt(line);
+        year = Number.parseInt(line, 10);
         break;
       case /^\d{1,2} \w+$/.test(line):
-        date = parse(year + " " + line, 'yyyy dd MMMM', new Date(2010, 0, 1))
+        date = parse(`${year} ${line}`, "yyyy dd MMMM", new Date(2010, 0, 1));
         rate = findRate(rates, date);
         // console.log('rate for date', date.toDateString(), rate);
         break;
       case /^Buy /.test(line):
         // console.log('buy', date.toDateString(), line);
-        stocks.push(parseStock('buy', line, date, rate));
+        stocks.push(parseStock("buy", line, date, rate));
         break;
       case /^Sell /.test(line):
         // console.log('sell', date.toDateString(), line);
-        stocks.push(parseStock('sell', line, date, rate));
+        stocks.push(parseStock("sell", line, date, rate));
         break;
       case /^Dividend /.test(line):
         // console.log('Dividend', date.toDateString(), line);
         devidends.push(parseDevidend(line, date, rate));
         break;
       default:
-        if(line.trim().length > 0) {
-          console.log('no case for line', line)
+        if (line.trim().length > 0) {
+          console.log("no case for line", line);
         }
     }
   }
@@ -153,27 +149,25 @@ async function parseTransactions(rates) {
   return {
     devidends,
     stocks
-  }
+  };
 }
 
 function calculateStocks(originalStocks) {
   const stocks = [...originalStocks].reverse();
   const buyTransactions = stocks
-    .filter(x => x.type === 'buy')
-    .map(t => ({ ...t, restShares: t.shares }));
-  const sellTransactions = stocks.filter(x => x.type === 'sell');
+    .filter((x) => x.type === "buy")
+    .map((t) => ({ ...t, restShares: t.shares }));
+  const sellTransactions = stocks.filter((x) => x.type === "sell");
   for (const sellTransaction of sellTransactions) {
     sellTransaction.bought = [];
-    // we need to find buy transaction for shares amount
+    // we need to find buy transaction for sold shares amount
     let sharesToFind = sellTransaction.shares;
     const buys = buyTransactions
-      .filter(buyT =>
-        buyT.company === sellTransaction.company &&
-        buyT.date <= sellTransaction.date
-      );
+      .filter((buyT) => buyT.company === sellTransaction.company
+        && buyT.date <= sellTransaction.date);
     for (const buyTransaction of buys) {
       if (sharesToFind === 0) break;
-      if (buyTransaction.restShares === 0) continue;
+      if (buyTransaction.restShares === 0) continue; // eslint-disable-line no-continue
       if (sharesToFind <= buyTransaction.restShares) {
         buyTransaction.restShares -= sharesToFind;
         sellTransaction.bought.push({
@@ -190,11 +184,12 @@ function calculateStocks(originalStocks) {
         buyTransaction.restShares = 0;
       }
     }
-    const boughtPrice = sellTransaction.bought.reduce((acc, x) => acc + x.pln * x.boughtShares / x.shares, 0)
-    sellTransaction.boughtPrice = round(boughtPrice)
-    sellTransaction.income = sellTransaction.pln - boughtPrice
+    const boughtPrice = sellTransaction.bought
+      .reduce((acc, x) => acc + (x.pln * x.boughtShares) / x.shares, 0);
+    sellTransaction.boughtPrice = round2(boughtPrice);
+    sellTransaction.income = sellTransaction.pln - boughtPrice;
     if (Math.abs(sharesToFind) > 0.0000001) {
-      console.log('cound not find shares', sharesToFind, sellTransaction.date.toLocaleDateString(), sellTransaction.company)
+      console.log("cound not where shares were bought", sharesToFind, sellTransaction.date.toLocaleDateString(), sellTransaction.company);
     }
   }
 
@@ -203,23 +198,27 @@ function calculateStocks(originalStocks) {
 
 function calcDevidents(devidends, year) {
   return devidends
-    .filter(d => d.date.getFullYear() === year)
-    .map(d => d.pln)
+    .filter((d) => d.date.getFullYear() === year)
+    .map((d) => d.pln)
     .reduce((acc, cur) => acc + cur, 0);
 }
 
 async function main() {
   const rates = await combineRates();
-  const transactions = await parseTransactions(rates);
+  const htmlTransactions = (await fs.readFile("stocks_transactions.txt", "utf8"))
+    .split("\n");
+  const transactions = await parseHtmlTransactions(rates, htmlTransactions);
 
   // const divedends = calcDevidents(transactions.devidends, 2021);
 
   const stocks = calculateStocks(transactions.stocks);
   // console.dir(stocks.filter(x => x.company === 'AAPL'), { depth: null });
   // console.dir(stocks.filter(x => x.type === 'sell' && x.date.getFullYear() === 2021));
-  console.log(stocks.filter(x => x.type === 'sell' && x.date.getFullYear() === 2021).reduce((acc, x) => acc + x.income, 0));
+  const income = stocks.filter((x) => x.type === "sell" && x.date.getFullYear() === 2021).reduce((acc, x) => acc + x.income, 0);
+  console.log("income", income);
+  console.log("taxes 19%", round2(round2(income) * 0.19));
+  // eslint-disable-next-line max-len
   // console.log(stocks.filter(x => x.company === 'TSLA').map(x => x.date.toLocaleDateString() + ": " + x.line).join('\n'))
 }
-
 
 main();
